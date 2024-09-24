90d3d9c11504e1c09c305ae4d75ea80eee70226b
Yoc Design <deepanshu@yoctel.com>
2024-09-24 12:19:21 +0530

    first commit

---
+++ footer.js
@@ -0,0 +1,10 @@
+document.write(`
+    <footer class="footer-area bg-color">
+            <div class="container">
+                <div class="footer-bottom-area">
+                    <p class='text-center'>Copyright <i class='bx bx-copyright'></i>2024 All Rights Reserved by
+                            <strong><a href="https://www.yoctel.com/" style='color:#4797ff;'>Yoctel</strong> Solutions Pvt Ltd</a></p>
+                </div>
+            </div>
+        </footer>
+    `);
---
+++ header.js
@@ -0,0 +1,91 @@
+document.write(`<div class="navbar-area">
+    <div class="tracer-responsive-nav">
+        <div class="container">
+            <div class="tracer-responsive-menu">
+                <div class="logo black-logo">
+                    <a href="index.html">
+                        <img src="https://www.yoctel.com/Content/assets/images/yoc-lg.jpg" alt="logo">
+                    </a>
+                </div>
+                <div class="logo white-logo">
+                    <a href="index.html">
+                        <img src="assets/img/white-logo.png" alt="logo">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tracer-nav">
+        <div class="container">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <a class="navbar-brand black-logo" href="index.html">
+                    <img src="https://www.yoctel.com/Content/assets/images/yoc-lg.jpg" alt="logo">
+                </a>
+                <a class="navbar-brand white-logo" href="index.html">
+                    <img src="https://www.yoctel.com/Content/assets/images/yoc-lg.jpg" alt="logo">
+                </a>
+
+                <div class="collapse navbar-collapse mean-menu">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="#Home" class="nav-link active">Home </a>
+                        </li>
+
+                        <li class="nav-item"><a href="#About-Us" class="nav-link">About Us </a>
+                        </li>
+
+                         <li class="nav-item"><a href="#best-Features" class="nav-link">Features </a>
+                        </li>
+
+                        <li class="nav-item"><a href="#testimonials" class="nav-link">Testimonial </a>
+                        </li>
+
+                        <li class="nav-item"><a href="#Faqs" class="nav-link">Faqs</a>
+                        </li>
+
+                        <li class="nav-item"><a href="#contact-us" class="nav-link">Contact Us </a>
+                       </li>
+                       
+
+                        
+
+
+                          
+                    </ul>
+
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="others-option-for-responsive">
+        <div class="container">
+            <div class="dot-menu">
+                <div class="inner">
+                    <div class="circle circle-one"></div>
+                    <div class="circle circle-two"></div>
+                    <div class="circle circle-three"></div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="option-inner">
+                    <div class="others-option">
+                        <div class="option-item">
+                            <form class="search-box">
+                                <input type="text" class="input-search" placeholder="Search for anything">
+                                <button type="submit"><i class="flaticon-loupe"></i></button>
+                            </form>
+                        </div>
+
+                        <div class="option-item">
+                            <a href="contact.html" class="default-btn"><i class="flaticon-right"></i>Get Started<span></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`)
